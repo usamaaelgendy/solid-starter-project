6d6a6f3e83d3acc1c50cdd7cbee70e3e9d6ab313
Usama Elgendy <usamaaelgendy@gmail.com>
2025-10-23 21:05:15 +0300

    Refactor file structure and add report generator class

---
+++ lib/Section 3 : OCP/3.1/report_generator_bad.dart
@@ -0,0 +1,58 @@
+class ReportData {
+  final String title;
+  final List<String> items;
+  final DateTime generatedAt;
+
+  ReportData({
+    required this.title,
+    required this.items,
+    required this.generatedAt,
+  });
+}
+
+class ReportGenerator {
+  String generate(ReportData data, String type) {
+    if (type == 'PDF') {
+      return _generatePDF(data);
+    } else if (type == 'HTML') {
+      return _generateHTML(data);
+    } else if (type == 'CSV') {
+      return _generateCSV(data);
+    } else if (type == 'EXCEL') {
+      return _generateExcel(data);
+    }
+
+    return 'Unsupported format';
+  }
+
+  String _generatePDF(ReportData data) {
+    return 'PDF Report: ${data.title}\n${data.items.join('\n')}';
+  }
+
+  String _generateHTML(ReportData data) {
+    return '<html><h1>${data.title}</h1><ul>${data.items.map((item) => '<li>$item</li>').join()}</ul></html>';
+  }
+
+  String _generateCSV(ReportData data) {
+    return '${data.title}\n${data.items.join(',')}';
+  }
+
+  String _generateExcel(ReportData data) {
+    return 'EXCEL:${data.title}|${data.items.join('|')}';
+  }
+}
+
+void main() {
+  final data = ReportData(
+    title: 'Sales Report',
+    items: ['Product A: \$100', 'Product B: \$200', 'Product C: \$150'],
+    generatedAt: DateTime.now(),
+  );
+
+  final generator = ReportGenerator();
+
+  print(generator.generate(data, 'PDF'));
+  print(generator.generate(data, 'HTML'));
+  print(generator.generate(data, 'EXCEL'));
+  print(generator.generate(data, 'JSON'));
+}
